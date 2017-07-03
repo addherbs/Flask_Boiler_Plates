77d98b61a596007e6b9e4134584136c1ef9008c6
addherbs <addherbs@gmail.com>
2017-07-03 13:17:56 -0500

    Final macros in jinja done

--- macro.py
+++ macro.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def test():
     testList = [{'title': '1'} , {'title': '2'}, {'title': '3'}]
-    return render_template('index.html', testList = testList)
+    lists = ['Aditya' , 'Devendra', 'Pandey', 'Parul' , 'Amrit' , 'Mummy']
+    return render_template('index.html', testList = testList, lists = lists)
 
 if __name__ == '__main__':
     app.run()
