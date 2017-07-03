86232d01bd9f1cebe2c8554e9ae7aaf57548bf4f
addherbs <addherbs@gmail.com>
2017-07-03 13:11:02 -0500

    Macro done

--- macro.py
+++ macro.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def test():
     testList = [{'title': '1'} , {'title': '2'}, {'title': '3'}]
-    return render_template('index.html', testList)
+    return render_template('index.html', testList = testList)
 
-if __name__ == __main__:
+if __name__ == '__main__':
     app.run()
